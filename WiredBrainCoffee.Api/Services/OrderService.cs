46690a8dcfad195fae3187b12802766066c1176d
Alex Wolf <alexrudiwolf@gmail.com>
2020-11-28 20:12:30 -0500

    fixed stuff

--- WiredBrainCoffee.Api/Services/OrderService.cs
+++ WiredBrainCoffee.Api/Services/OrderService.cs
@@ -33,16 +33,30 @@ namespace WiredBrainCoffee.Api.Services
                 "Pumpkin Bread"
             };
 
+            var sampleLorem = new List<string>()
+            {
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+                "Ut posuere fringilla ipsum ac eleifend.",
+                "Quisque malesuada purus non justo viverra tristique.",
+                "Vestibulum vestibulum laoreet magna sed tempor.",
+                "Duis tempus auctor tellus, vitae accumsan lectus blandit vitae.",
+                "Etiam et erat eu massa accumsan dignissim nec nec neque.",
+                "Vivamus consequat dui vel pellentesque malesuada.",
+                "Fusce tincidunt ante et dolor posuere pellentesque.",
+                "Praesent vehicula condimentum nunc. Etiam mattis non quam ut eleifend.",
+                "Pellentesque habitant morbi tristique senectus et netus"
+            };
+
             Random rnd = new Random();
             for (int i = 0; i < 1000; i++)
             {
                 generatedOrders.Add(new Order()
                 {
                     Id = i,
-                    Created = DateTime.Now,
+                    Created = DateTime.Now.AddHours(rnd.Next(1,4)).AddDays(rnd.Next(1, 20)),
                     OrderNumber = rnd.Next(1000, 10000),
                     PromoCode = "None",
-                    Notes = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+                    Notes = sampleLorem[rnd.Next(0, 9)],
                     Items = new List<OrderItem>() { new OrderItem() { Id = 1, Name = sampleItems[rnd.Next(0, 9)], Price = 5 } }
                 });
             }
