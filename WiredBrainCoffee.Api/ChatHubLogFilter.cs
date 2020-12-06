843c4aa8b8d3d7bd8deb985f5ebc8cd92da33f55
Alex Wolf <alexrudiwolf@gmail.com>
2020-12-06 13:12:25 -0500

    rename filter

---
+++ WiredBrainCoffee.Api/ChatHubLogFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.Api
+{
+    public class ChatHubLogFilter : IHubFilter
+    {
+        public async ValueTask<object> InvokeMethodAsync(
+        HubInvocationContext invocationContext, Func<HubInvocationContext, ValueTask<object>> next)
+        {
+            Debug.WriteLine($"Conversation Log: '{JsonConvert.SerializeObject(invocationContext.HubMethodArguments)}'");
+
+            Debug.WriteLine("Before hub execution");
+                
+            var nextRun = await next(invocationContext);
+
+            Debug.WriteLine("After hub exection");
+
+            return nextRun;
+        }
+    }
+}
