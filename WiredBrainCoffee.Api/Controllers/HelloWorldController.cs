7078d3acad7d42c87c4277f61622f81823925e5c
Alex Wolf <alexrudiwolf@gmail.com>
2020-12-06 12:42:37 -0500

    finish

---
+++ WiredBrainCoffee.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WiredBrainCoffee.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetGreeting()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
